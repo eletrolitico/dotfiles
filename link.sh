d5602ebcbba63b66cd5546fac312d9b72083b9e6
Henrique Possatto <henriquempossatto@gmail.com>
2021-03-09 12:38:07 -0300

    dev

--- link.sh
+++ link.sh
@@ -12,6 +12,12 @@ link_folder()
   done
 }
 
+if [ -d ".oh-my-zsh/custom/themes/powerlevel10k" ]; then
+  echo "powerlevel10k já clonado"
+else
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git .oh-my-zsh/custom/themes/powerlevel10k
+fi
+
 link ".oh-my-zsh"
 link ".xinitrc"
 link ".Xresources"
