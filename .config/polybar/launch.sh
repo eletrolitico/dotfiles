204729c12363e855666fa6e178d94dc0eb6b44e3
Henrique Possatto <henriquempossatto@gmail.com>
2021-03-11 14:58:29 -0300

    ajuste polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,8 +12,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
+polybar main -c ~/.config/polybar/config.ini > /tmp/log_pbar_1.txt 2>&1 &
 
 if [[ -n $MONITOR2 ]]; then
-    MONITOR="$MONITOR2" polybar -c ~/.config/polybar/config.ini secondary &
+    MONITOR="$MONITOR2" polybar -c ~/.config/polybar/config.ini secondary > /tmp/log_pbar_2.txt 2>&1 &
 fi
