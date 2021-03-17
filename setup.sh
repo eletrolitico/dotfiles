afb27f8c7f4cb41854183816d61476157d2d2bb3
Henrique Possatto <henriquempossatto@gmail.com>
2021-03-17 15:06:58 -0300

    updated script

--- setup.sh
+++ setup.sh
@@ -2,131 +2,94 @@
 
 link()
 {
-  echo "ln -sfT ../dotfiles/$1 ../$1"
-  ln -sfT dotfiles/$1 ../$1
+    echo "ln -sfT ../dotfiles/$1 ../$1"
+    ln -sfT dotfiles/$1 ../$1
 }
 
 link_folder()
 {
-  for f in $1/*; do
-    echo "linking $f"
-    ln -sfT ../dotfiles/$f ../$f
-  done
+    for f in $1/*; do
+        echo "linking $f"
+        ln -sfT ../dotfiles/$f ../$f
+    done
 }
 
 print_array()
 {
-  arr=("$@")
-  i=0
-  for e in "${arr[@]}"; do
-    echo "$i: $e"
-    i=$((i+1))
-  done
+    arr=("$@")
+    i=0
+    for e in "${arr[@]}"; do
+        echo "$i: $e"
+        i=$((i+1))
+    done
 }
 
 if  [ $# -ne 1 ] || [[ $1 != "link" && $1 != "install" && $1 != "all" ]] ; then
-  echo "Options:"
-  echo "  link - symlinks config files"
-  echo "  install - installs packages (including yay)"
-  echo "  all - symlinks configs and install packages"
-
-  exit 1
+    echo "Options:"
+    echo "  link - symlinks config files"
+    echo "  install - installs packages (including yay)"
+    echo "  all - symlinks configs and install packages"
+    
+    exit 1
 fi
 
 if [ $1 == "link" ] || [ $1 == "all" ]; then
-  if [ ! -f ".oh-my-zsh/oh-my-zsh.sh" ]; then
-    rm -rf .oh-my-zsh
-    git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
-  fi
-  if [ -d ".oh-my-zsh/custom/themes/powerlevel10k" ]; then
-    echo "powerlevel10k já clonado"
-  else
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git .oh-my-zsh/custom/themes/powerlevel10k
-  fi
-
-  XRAN_FILE="xrand.sh"
-
-  if [ ! -f $XRAN_FILE ]; then
-    MONITORS=($(xrandr | grep ".* connected" | cut -d ' ' -f1))
-    print_array ${MONITORS[@]}
-    printf "Choose the primary monitor(type the number): "
-    read prM
-    printf 'xrandr' > $XRAN_FILE
-
-    MODES=("--right-of" "--left-of" "--above" "--below")
-    I=0
-    for m in "${MONITORS[@]}"; do
-
-      if [ $I -eq $prM ]; then
-        echo -ne " --output ${MONITORS[I]} --primary --auto" >> $XRAN_FILE
-      else
-
-        printf "\n\n${MONITORS[I]} is:\n"
-        printf "0: right-of\n1: left-of\n2: above\n3: below\noption: "
-        read mod
-
-        printf "\n\nwhich monitor:\n"
-        print_array ${MONITORS[@]}
-        printf "option: "
-        read mon
-
-        echo -ne " --output ${MONITORS[I]} --auto ${MODES[mod]} ${MONITORS[mon]}" >> $XRAN_FILE
-      fi
-      I=$(($I+1))
-    done
-  fi
-
-  mkdir -p $HOME/Pics/wallpapers
-
-  link ".oh-my-zsh"
-  link ".xinitrc"
-  link ".Xresources"
-  link ".xscreensaver"
-  link ".zshrc"
-  link ".p10k.zsh"
-  link_folder ".config"
-  link_folder ".local"
+    if [ ! -f ".oh-my-zsh/oh-my-zsh.sh" ]; then
+        rm -rf .oh-my-zsh
+        git clone https://github.com/ohmyzsh/ohmyzsh.git .oh-my-zsh
+    fi
+    if [ -d ".oh-my-zsh/custom/themes/powerlevel10k" ]; then
+        echo "powerlevel10k já clonado"
+    else
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git .oh-my-zsh/custom/themes/powerlevel10k
+    fi
+    
+    mkdir -p $HOME/Pics/wallpapers
+    
+    link ".oh-my-zsh"
+    link ".zshrc"
+    link ".p10k.zsh"
+    link_folder ".config"
+    link_folder ".local"
 fi
 
 if [ $1 == "install" ] || [ $1 == "all" ]; then
-  sudo pacman -Syyuu --noconfirm
-  sudo pacman -S i3-gaps \
-  alacritty \
-  pulseaudio \
-  pavucontrol \
-  flameshot \
-  nm-applet \
-  xorg \
-  xorg-xinit \
-  xscreensaver \
-  zsh \
-  go \
-  blueman \
-  feh \
-  openssh \
-  docker \
-  docker-compose \
-  gnome-keyring \
-  network-manager-applet \
-  --noconfirm
-
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si --noconfirm
-  cd ..
-  rm -rf yay
-
-  yay -S polybar \
-  deadd-notification-center-bin \
-  visual-studio-code-bin \
-  mpris-ctl \
-  xbanish \
-  snixembed-git \
-  picom-jonaburg-git \
-  spotify \
-  google-chrome \
-  nerd-fonts-jetbrains-mono \
-  nerd-fonts-source-code-pro \
-  nerd-fonts-fira-code \
-  --noconfirm
+    sudo pacman -Syyuu --noconfirm
+    sudo pacman -S --needed sway \
+    alacritty \
+    grim \
+    i3status \
+    mako \
+    slurp \
+    swayidle \
+    swaylock \
+    wallutils \
+    waybar \
+    xorg-server-xwayland \
+    pulseaudio \
+    pavucontrol \
+    zsh \
+    go \
+    blueman \
+    openssh \
+    docker \
+    docker-compose \
+    gnome-keyring \
+    network-manager-applet \
+    playerctl \
+    --noconfirm
+    
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay
+    
+    yay -S google-chrome \
+    visual-studio-code-wayland \
+    spotify \
+    nerd-fonts-jetbrains-mono \
+    nerd-fonts-source-code-pro \
+    nerd-fonts-fira-code \
+    --noconfirm
 fi
